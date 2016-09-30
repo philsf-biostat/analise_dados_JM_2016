f32a229b02d50ca9bf1816003cf90f61103229f6
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2016-09-30 18:37:34 -0300

    implementação do bridging 0.5.2

--- Jessica-PCR-Tit.R
+++ Jessica-PCR-Tit.R
@@ -2,3 +2,13 @@
 # df <- read.xlsx("PCRxTITULACAO.xlsx",1)
 # write.csv2(df[c(1,4,3)], "PCRxTITULACAO.csv", row.names = F)
 
+df <- read.csv2("PCRxTITULACAO.csv")
+library(MethComp)
+mcdf <- Meth(df, meth = "Metodo", y = "Stuff", item = "Coleta")
+levels(mcdf$meth) <- list(REF = "Titulacao", NEW = "PCR")
+source("../Documents/LATEV/bridging/bridging-analysis.R")
+source("../Documents/LATEV/bridging/plots.R")
+source("../Documents/LATEV/bridging/cleanup.R")
+
+qual.cutoff <- 2
+quant.results
