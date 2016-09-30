86c24101849a0cb35a87eca5ba94d3103e15c316
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2016-09-30 18:48:35 -0300

    opção de trabalhar na escala log (exige brid novo)
    Para oferecer a opção de trabalhar com dado já na escala logarítmica, é
    necessário adaptar o bridging para copiar os dados sem
    transformá-los.

--- Jessica-PCR-Tit.R
+++ Jessica-PCR-Tit.R
@@ -1,10 +1,12 @@
 # library(xlsx)
 # df <- read.xlsx("PCRxTITULACAO.xlsx",1)
 # write.csv2(df[c(1,4,3)], "PCRxTITULACAO.csv", row.names = F)
+# write.csv2(df, "PCRxTITULACAO.csv", row.names = F)
 
 df <- read.csv2("PCRxTITULACAO.csv")
 library(MethComp)
 mcdf <- Meth(df, meth = "Metodo", y = "Stuff", item = "Coleta")
+# mcdf <- Meth(df, meth = "Metodo", y = "Stuff.log", item = "Coleta")
 levels(mcdf$meth) <- list(REF = "Titulacao", NEW = "PCR")
 source("../Documents/LATEV/bridging/bridging-analysis.R")
 source("../Documents/LATEV/bridging/plots.R")
