fd7efc80c9541c86d22e375912c05952e25c7a19
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2016-12-01 16:08:43 -0200

    rascunho atualizado para novos dados

--- Jessica-PCR-Tit.R
+++ Jessica-PCR-Tit.R
@@ -3,11 +3,19 @@
 # write.csv2(df[c(1,4,3)], "PCRxTITULACAO.csv", row.names = F)
 # write.csv2(df, "PCRxTITULACAO.csv", row.names = F)
 
-df <- read.csv2("PCRxTITULACAO.csv")
+# df <- read.csv2("PCRxTITULACAO.csv")
+
+library(xlsx)
+
+df <- read.xlsx("dataset/Dados_PCR_Tit.xlsx", 1)
+
+df <- read.xlsx("dataset/Dados_PCR_Tit.xlsx", 3)
+
+
 library(MethComp)
-mcdf <- Meth(df, meth = "Metodo", y = "Stuff", item = "Coleta")
+mcdf <- Meth(df, meth = "Metodo", y = "Quantidade", item = "Coleta")
 # mcdf <- Meth(df, meth = "Metodo", y = "Stuff.log", item = "Coleta")
-levels(mcdf$meth) <- list(REF = "Titulacao", NEW = "PCR")
+levels(mcdf$meth) <- list(REF = "Titulacao", NEW = "qPCR")
 source("../Documents/LATEV/bridging/bridging-analysis.R")
 source("../Documents/LATEV/bridging/plots.R")
 source("../Documents/LATEV/bridging/cleanup.R")
