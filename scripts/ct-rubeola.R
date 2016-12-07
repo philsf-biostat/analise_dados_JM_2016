ba7a1440764be64e79699399d8e0fc9f034b39a7
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2016-12-07 15:23:21 -0200

    Duplicata removida

--- scripts/ct-rubeola.R
+++ scripts/ct-rubeola.R
@@ -4,8 +4,8 @@ attach(df)
 # Objetivo: observar o ajuste do virus puro com as combinações biplex.
 # Critério: R^2
 
-png("figuras/ct-4-graphs.png", 700, 700)
-par(mfrow = c(2,2))
+png("figuras/ct-4-graphs.png", 700, 350)
+par(mfrow = c(1,2))
 
 plot(CR.DILUIDO ~ Rp, xlim=c(0,40), ylim=c(0,40))
 rp.cs.dil <- lm(CR.DILUIDO ~ Rp)
@@ -18,20 +18,6 @@ plot(CR.CONCENTRADO ~ Rp, xlim=c(0,40), ylim=c(0,40))
 rp.cs.conc <- lm(CR.CONCENTRADO ~ Rp)
 abline(rp.cs.conc)
 r2 <- paste('R^2 =', round(summary(rp.cs.conc)$r.squared, 4))
-text(15,2, r2)
-rm(r2)
-
-plot(CR.DILUIDO ~ Rp, xlim=c(0,40), ylim=c(0,40))
-rp.cr.dil <- lm(CR.DILUIDO ~ Rp)
-abline(rp.cr.dil)
-r2 <- paste('R^2 =', round(summary(rp.cr.dil)$r.squared, 4))
-text(15,2, r2)
-rm(r2)
-
-plot(CR.CONCENTRADO ~ Rp, xlim=c(0,40), ylim=c(0,40))
-rp.cr.conc <- lm(CR.CONCENTRADO ~ Rp)
-abline(rp.cr.conc)
-r2 <- paste('R^2 =', round(summary(rp.cr.conc)$r.squared, 4))
 text(15,2, r2)
 rm(r2)
 
