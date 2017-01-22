cbc49f97d7c757aba5a16d7e1a4079351691d6dc
Felipe Figueiredo <philsf79@gmail.com>
2017-01-22 21:33:30 -0200

    myplot(): função para padronizar todos os gráficos
    Features:
    
    - xlim e ylim
    - xlab e ylab
    - regressão linear
    - (text) equação da regressão linear
    - (text) R^2

---
+++ scripts/myplot.R
@@ -0,0 +1,19 @@
+myplot <- function(x, y, xlim = NULL, ylim = NULL, xlab = NULL, ylab = NULL) {
+  x.name <- deparse(substitute(x))
+  y.name <- deparse(substitute(y))
+  if (is.null(xlab)) xlab <- x.name
+  if (is.null(ylab)) ylab <- y.name
+  modelo <- lm(y ~ x)
+  r2 <- paste('R^2 =', round(summary(modelo)$r.squared, 4))
+  coefs <- round(modelo$coefficients, 4)
+  eqn <- paste(y.name,"=",coefs[2],x.name,"+",coefs[1])
+
+  plot(y ~ x, xlim = xlim, ylim = ylim,
+       xlab = xlab,
+       ylab = ylab,
+       # main = "Mix qPCR Diluído"
+       )
+  abline(modelo)
+  text(23,15, eqn)
+  text(25, 12, r2)
+}
