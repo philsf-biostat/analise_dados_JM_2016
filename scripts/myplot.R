4209f214f22e4670753d254188851c1df34b5a97
Felipe Figueiredo <philsf79@gmail.com>
2017-01-22 21:45:31 -0200

    myplot: oferecer digits

--- scripts/myplot.R
+++ scripts/myplot.R
@@ -1,11 +1,11 @@
-myplot <- function(x, y, xlim = NULL, ylim = NULL, xlab = NULL, ylab = NULL) {
+myplot <- function(x, y, xlim = NULL, ylim = NULL, xlab = NULL, ylab = NULL, digits = 4) {
   x.name <- deparse(substitute(x))
   y.name <- deparse(substitute(y))
   if (is.null(xlab)) xlab <- x.name
   if (is.null(ylab)) ylab <- y.name
   modelo <- lm(y ~ x)
-  r2 <- paste('R^2 =', round(summary(modelo)$r.squared, 4))
-  coefs <- round(modelo$coefficients, 4)
+  r2 <- paste('R^2 =', round(summary(modelo)$r.squared, digits))
+  coefs <- round(modelo$coefficients, digits)
   eqn <- paste(y.name,"=",coefs[2],x.name,"+",coefs[1])
 
   plot(y ~ x,
