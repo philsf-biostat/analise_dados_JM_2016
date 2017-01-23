18af5f665f42a53b3155f3ebcebf816d62afd195
Felipe Figueiredo <philsf79@gmail.com>
2017-01-22 23:40:48 -0200

    upgrade: Ct de sarampo agora com myplot

--- scripts/ct-sarampo.R
+++ scripts/ct-sarampo.R
@@ -8,25 +8,15 @@ source("scripts/myplot.R")
 png("figuras/ct-sarampo.png", 700, 350)
 par(mfrow = c(1,2))
 
-plot(CS.DILUIDO ~ Sp, xlim=c(0,40), ylim=c(0,40),
-     xlab = "Ct Sarampo (puro)",
-     ylab = "Ct Caxumba + Sarampo",
-     main = "Mix qPCR Diluído")
-sp.cs.dil <- lm(CS.DILUIDO ~ Sp)
-abline(sp.cs.dil)
-r2 <- paste('R^2 =', round(summary(sp.cs.dil)$r.squared, 4))
-text(15,2, r2)
-rm(r2)
+with(df, myplot(Sp, CS.DILUIDO, xlim=c(0,40), ylim=c(0,40),
+                xlab = "Ct Sarampo (puro)",
+                ylab = "Ct Caxumba + Sarampo"))
+title(main = "Mix qPCR Diluído")
 
-plot(CS.CONCENTRADO ~ Sp, xlim=c(0,40), ylim=c(0,40),
-     xlab = "Ct Sarampo (puro)",
-     ylab = "Ct Caxumba + Sarampo",
-     main = "Mix qPCR Concentrado")
-sp.cs.conc <- lm(CS.CONCENTRADO ~ Sp)
-abline(sp.cs.conc)
-r2 <- paste('R^2 =', round(summary(sp.cs.conc)$r.squared, 4))
-text(15,2, r2)
-rm(r2)
+with(df, myplot(Sp, CS.CONCENTRADO, xlim=c(0,40), ylim=c(0,40),
+                xlab = "Ct Sarampo (puro)",
+                ylab = "Ct Caxumba + Sarampo"))
+title(main = "Mix qPCR Concentrado")
 
 dev.off()
 
