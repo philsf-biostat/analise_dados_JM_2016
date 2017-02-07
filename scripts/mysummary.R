9a0d6fde5b23c6d28d4934619a1679ec61435d1e
Felipe Figueiredo <philsf79@gmail.com>
2017-02-06 23:34:55 -0200

    mysummary: usar teste t pareado

--- scripts/mysummary.R
+++ scripts/mysummary.R
@@ -1,5 +1,5 @@
 mysummary <- function(x, digits = 2) {
-  tt <- with(x, t.test(qPCR - Titulacao))
+  tt <- with(x, t.test(qPCR, Titulacao, paired = T))
   r <- with(x, cor(qPCR, Titulacao))
   mysummary <- list(Média = tt$estimate, IC = tt$conf.int, r = r)
   mysummary <- lapply(mysummary, round, digits)
