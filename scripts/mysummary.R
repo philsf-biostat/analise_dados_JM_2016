4ab630224ed661ae0cba8dff5c9e2ebb70be5396
Felipe Figueiredo <philsf79@gmail.com>
2017-02-06 23:10:15 -0200

    usar o nome como rowname, ao invés de uma coluna própria

--- scripts/mysummary.R
+++ scripts/mysummary.R
@@ -1,6 +1,8 @@
 mysummary <- function(x) {
   tt <- with(x, t.test(qPCR - Titulacao))
   r <- with(x, cor(qPCR, Titulacao))
-  mysummary <- list(deparse(substitute(x)), Média = tt$estimate, IC = tt$conf.int, r = r)
-  (mysummary <- t(mysummary))
+  mysummary <- list(Média = tt$estimate, IC = tt$conf.int, r = r)
+  mysummary <- t(mysummary)
+  rownames(mysummary) <- deparse(substitute(x))
+  (mysummary)
 }
