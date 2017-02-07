b695ebb8a62d40b342891c344023d124b8faa886
Felipe Figueiredo <philsf79@gmail.com>
2017-02-06 23:13:21 -0200

    oferecer digits (default = 2)

--- scripts/mysummary.R
+++ scripts/mysummary.R
@@ -1,7 +1,8 @@
-mysummary <- function(x) {
+mysummary <- function(x, digits = 2) {
   tt <- with(x, t.test(qPCR - Titulacao))
   r <- with(x, cor(qPCR, Titulacao))
   mysummary <- list(Média = tt$estimate, IC = tt$conf.int, r = r)
+  mysummary <- lapply(mysummary, round, digits)
   mysummary <- t(mysummary)
   rownames(mysummary) <- deparse(substitute(x))
   (mysummary)
